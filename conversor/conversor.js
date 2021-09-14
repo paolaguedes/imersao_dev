2304328b5d8b4848b864182373fe4b27cbc0a833
PaolaGuedes <paolaguedesbacca2002@gmail.com>
2021-09-14 14:14:14 -0300

    conversor

---
+++ conversor/conversor.js
@@ -0,0 +1,17 @@
+function Converter() {
+
+    var valorElemento = document.getElementById("valor")
+    var valor = valorElemento.value
+    var valorDolar = parseFloat(valor)
+
+    var valorReal = valorDolar * 5
+
+    var elementoValorConvertido = document.getElementById("valorConvertido")
+    var valorConvertido = "O resultado em real é R$ " + valorReal
+    elementoValorConvertido.innerHTML = valorConvertido
+}
+
+// Adicionar outras moedas para converter.
+// Conversor de quilômetros para anos luz e verificar o tempo que demora para ir de uma estrela para outra.
+// Conversor de temperaturas entre farenheit, kelvin e celcius.
+// Adicionar uma linha ao projeto desenvolvido para que apareça o valor em bitcoin.
