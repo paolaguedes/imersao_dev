db923436d46c74c2ea7e0d42087478d696468d17
PaolaGuedes <paolaguedesbacca2002@gmail.com>
2021-09-16 15:42:36 -0300

    alterações

--- calculadora_media/calcula.js
+++ calculadora_media/calcula.js
@@ -8,18 +8,17 @@ var notaSegundo = parseFloat(document.getElementById("nota2").value)
 var notaFinal = (notaPrimeiro + notaSegundo) / 2
 
 //define a quantidade de casas decimais após a virgula
-notaFinal = notaFinal.toFixed(2)
+notaFinal = parseFloat(notaFinal.toFixed(2))
 
-notaFinal = parseFloat(notaFinal)
 var resultadoFinal = document.getElementById("resultado")
+var resultado
 
 if (notaFinal >= 5) {
-    var resultado = "Parabens, sua nota final foi "+notaFinal+ ", "+nome+". Você está aprovado."
-    resultadoFinal.innerHTML = resultado
+    resultado = "Parabens, sua nota final foi "+notaFinal+ ", "+nome+". Você está aprovado."
 }
 else {
-    var resultado = nome +" sua nota foi: "+notaFinal+", mas não se preocupe. Uma nota não te define."
-    // leva o valor da variavel resultado para o HTML
-    resultadoFinal.innerHTML = resultado
+    resultado = nome +" sua nota foi: "+notaFinal+", mas não se preocupe. Uma nota não te define."
 }
+// leva o valor da variavel resultado para o HTML
+resultadoFinal.innerHTML = resultado
 }
--- mentalista/mentalista.js
+++ mentalista/mentalista.js
@@ -1,7 +1,23 @@
-var numeroSecreto = 3;
+//biblioteca que gera numeros aleatórios
+var numeroSecreto = parseInt(Math.random() * 11)
 
 function Adivinha() {
-    var numeroChutado = parseInt(document.getElementById("numero").value)
+    var chute = parseInt(document.getElementById("numero").value) 
+    var inputResultado = document.getElementById("numeroChutado")
+    var resultado
 
-    console.log(numeroChutado)
+            if (chute == numeroSecreto){
+                resultado =  "Você acertou o número secreto!!"
+            }
+            else if (chute < 0 || chute > 10){
+                resultado = "Digite um número no intervalo de 0 a 10!"
+            }            
+            else if (chute < numeroSecreto) {
+                resultado =  "Você chutou baixo!!"
+            }
+            else if (chute > numeroSecreto) {
+                resultado =  "Você chutou alto!!"            
+            }
+
+        inputResultado.innerHTML = resultado
 }
